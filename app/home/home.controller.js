b247014fa1ed944026d9163e059b1d1a4e7d691a
Alex <amcel1288@gmail.com>
2016-11-18 07:23:29 -0800

    first commit

---
+++ app/home/home.controller.js
@@ -0,0 +1,58 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = [];
+
+    /* @ngInject */
+    function HomeController() {
+        var vm = this;
+        vm.things = [];
+        vm.addThing = addThing;
+
+     	///////////////
+     	
+     	vm.classSort = classSort;
+     	vm.revClassSort = revClassSort;
+     	vm.textSort = textSort;
+     	vm.typeSort = '';
+        function addThing() {
+        	var obj = {
+        		text: vm.newThing, 
+        		priority: vm.newPriority
+        	}
+        	switch (obj.priority) {
+        		case '1':
+        			obj.class = "list-group-item-danger"
+        			break;
+        		case '2':
+        			obj.class = "list-group-item-warning"
+        			break;
+        		case '3':
+        			obj.class = "list-group-item-info"
+        			break;
+        		}
+        	vm.things.push(obj);
+        	
+        };
+        function classSort() {
+        	vm.typeSort = 'priority';
+        }
+        function revClassSort() {
+        	vm.typeSort = '-priority';
+        }
+        function textSort() {
+        	vm.typeSort = 'text';
+        }
+    }
+})();
+
+
+
+
+// list-group-item-danger
+// list-group-item-info
+// list-group-item-warning
